afa1d5424e080822e36c1e4c5f2c8624e35b7594
Paul Robinson <paulalex675@hotmail.com>
2021-07-23 02:03:39 +0100

    basic template done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :clients
   devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  root to: 'home#index'
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
